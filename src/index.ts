28933ada7b747b70f74edb4913fb73827a47ca31
merlin1320 <littlenights447@gmail.com>
2025-04-29 21:30:58 -0600

    Setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
-import cors from 'cors';
+import cors from "cors";
+import { randomUUID } from "crypto";
 
 const app = express();
 const port = 3020;
@@ -15,17 +16,17 @@ interface Preferences {
   text: boolean;
   email: boolean;
   phone: boolean;
-favoriteColors: string[];
+  favoriteColors: string[];
 }
 
 const corsOptions = {
-    origin: '*', // Allow all origins (not recommended for production)
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Allow cookies and authorization headers
-  };
+  origin: "*", // Allow all origins (not recommended for production)
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Allow cookies and authorization headers
+};
 
-  app.use(cors(corsOptions));
-  app.use(express.json());
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, Express with TypeScript!");
