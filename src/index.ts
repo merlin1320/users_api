3b122e57eab470e5471aa78442f3b955d0c4ac4a
merlin1320 <littlenights447@gmail.com>
2025-04-29 21:28:09 -0600

    Updated interface and cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
 import express, { Request, Response } from "express";
+import cors from 'cors';
 
 const app = express();
 const port = 3020;
+
+interface User {
+  id: string;
+  username: string;
+  preferences: Preferences;
+}
+
+interface Preferences {
+  lightdark: boolean;
+  text: boolean;
+  email: boolean;
+  phone: boolean;
+favoriteColors: string[];
+}
+
+const corsOptions = {
+    origin: '*', // Allow all origins (not recommended for production)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow cookies and authorization headers
+  };
+
+  app.use(cors(corsOptions));
+  app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, Express with TypeScript!");
